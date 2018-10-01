4bcd320e870f4d17526c2e8afc0a0fda5d521c84
Jake Archibald <jaffathecake@gmail.com>
2018-10-01 08:50:37 +0100

    Another fix

--- deploy.sh
+++ deploy.sh
@@ -54,10 +54,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy-key.enc -out ../deploy-key -d
+chmod 600 ../deploy-key
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add deploy-key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
