932647092c46fdfa585e61a49274f7afd43afc12
Jake Archibald <jaffathecake@gmail.com>
2018-10-01 08:56:53 +0100

    Please. My build script. He's very sick.

--- deploy.sh
+++ deploy.sh
@@ -57,7 +57,7 @@ ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy-key.enc -out ../deploy-key -d
 chmod 600 ../deploy-key
 eval `ssh-agent -s`
-ssh-add deploy-key
+ssh-add ../deploy-key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
